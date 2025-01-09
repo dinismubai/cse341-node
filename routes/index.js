fd2fc91d65fda1a9ffdf7c7a902dcc3f9b028214
dinismubai <mubydinis@gmail.com>
2025-01-09 21:10:53 +0200

    last work before deploying

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 
-router.get('/', (req, res) => {res.send('Hello World')});
+//router.get('/', (req, res) => {res.send('Hello World')});
 
-module.exports = router;
+router.use('/users', require('./users'));
+
+module.exports = router; 
