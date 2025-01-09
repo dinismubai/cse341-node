240463bf0f549f3f565bebec48e4514352891ba9
dinismubai <mubydinis@gmail.com>
2025-01-09 16:17:27 +0200

    Added routes folder

--- server.js
+++ server.js
@@ -2,11 +2,8 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello Mubai!');
-});
+const port = process.env.port || 3000;
 
-const port = 3000;
+app.use('/', require('./routes'));
 
-app.listen(process.env.port || port);
-console.log('Web Server is listening at port ' + (process.eventNames.port || port));
+app.listen(port, () => {console.log(`Running on port ${port}`)});
