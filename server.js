ae4bcf34d9ddc487829a8aef9975bedd155d007e
dinismubai <mubydinis@gmail.com>
2025-01-17 01:47:46 +0200

    Swagger version

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 //express webserver
 const express = require('express');
+const bodyParser = require('body-parser');
 const mongodb = require('./data/database');
 const app = express();
 
-const port = process.env.port || 3000;
+const port = process.env.port || 3000; 
+app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+});
 app.use('/', require('./routes'));
+
 
 mongodb.initDb((err) => {
     if(err)
